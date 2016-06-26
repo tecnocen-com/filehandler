75eafbe65e6b610cbc5105237af471e67db966c8
Angel Guevara <aguevara@tecnocen.com>
2016-06-26 14:29:30 -0500

    esqueleto de behavior

---
+++ behaviors/FileHandlerBehavior.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tecnocen\filehandler\validators;
+
+class FileHandlerBehavior extends \yii\base\Behavior
+{
+    public $rule = [];
+    public function getFilePath()
+    {
+    }
+
+    public function getFileName()
+    {
+    }
+
+    public function getMimeType()
+    {
+    }
+
+    public function attachValdator($event)
+    {
+        // check if 
+    }
+}
