ba28e465c6c98cca7d93510a7da0f5675a107f78
Angel Guevara <aguevara@tecnocen.com>
2016-07-04 03:11:53 -0500

    :book: documentation for the ActiveDownloadAction

--- actions/ActiveDownloadAction.php
+++ actions/ActiveDownloadAction.php
@@ -6,18 +6,89 @@ use Yii;
 use yii\base\InvalidConfigException;
 use yii\db\BaseActiveRecord;
 
+/**
+ * Handles the download of a file obtained from an active record.
+ *
+ * It must be configured on the `\yii\web\Controller::actions()` method.
+ *
+ * ```php
+ * public function actions()
+ * {
+ *     return [
+ *         'download' => [
+ *             'class' => ActiveDownloadAction::className(),
+ *             // Optionals.
+ *             'modelClass' => $this->modelClass,
+ *             'findModel' => [$this, 'findModel'],
+ *             'checkAccess' => [$this, 'checkAccess']
+ *         ]
+ *     ];
+ * }
+ * ```
+ *
+ * @param string $modelClass
+ * @param boolean $forceDownload
+ * @param string $forceDownloadParam
+ * @param callable $findModel
+ * @param callable $checkAccess
+ * @author Angel (Faryshta) Guevara <aguevara@tecnocen.com>
+ */
 class ActiveDownloadAction extends \yii\web\Action
 {
+    /**
+     * @var string
+     */
     public $modelClass;
 
+    /**
+     * @var boolean if
+     */
     public $forceDownload = false;
 
+    /**
+     * @var string parameter in the url that decides if the file must be
+     * downloaded without asking.
+     *
+     * if `forceDownload` is set to `true` then this parameter will be ignored.
+     */
     public $forceDownloadParam = 'forceDownload';
 
+    /**
+     * @var callable function to find the model
+     *
+     * It must have the signature
+     *
+     * ```php
+     * function (mixed $id): BaseActiveRecord
+     * ```
+     *
+     * Where:
+     *
+     * - $id is a mixed parameter
+     *
+     * and must return an instance of BaseActiveRecord
+     */
     public $findModel;
 
+    /**
+     * @var callable checks if the logged or anonymous user can access the
+     * model or throws exceptions otherwise.
+     *
+     * It must have the signature
+     *
+     * ```php
+     * function (BaseActiveRecord $model)
+     * ```
+     *
+     * Where:
+     *
+     * - $model is an instance of `BaseActiveRecord`
+     */
     public $checkAccess;
 
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         if ($this->findModel !== null && !is_callable($this->findModel)) {
@@ -48,6 +119,14 @@ class ActiveDownloadAction extends \yii\web\Action
         }
     }
 
+    /**
+     * Finds the model to be operated base on the id.
+     *
+     * @param mixed $id
+     * @return BaseActiveRecord
+     * @see $modelClass
+     * @see $findModel
+     */
     protected function findModel($id)
     {
         if ($this->findModel !== null) {
@@ -61,13 +140,38 @@ class ActiveDownloadAction extends \yii\web\Action
         return $modelClass::findOne($id);
     }
 
-    protected function checkAccess($model)
+    /**
+     * Checks if the user has the permissions to acces the model.
+     *
+     * @param BaseActiveRecord $model
+     * @see $checkAccess
+     */
+    protected function checkAccess(BaseActiveRecord $model)
     {
         if ($this->checkAccess !== null) {
             call_user_func($this->checkAccess, $model);
         }
     }
 
+    /**
+     * @return boolean if the file will be downloaded directly without asking
+     */
+    protected function forceDownload()
+    {
+        return $this->forceDownload
+            || (bool) Yii::$app->request->get(
+                $this->forceDownloadParam,
+                false
+            );
+    }
+
+    /**
+     * Find the model and returns the file using the
+     * `\yii\web\Response::sendFile()` method
+     *
+     * @param mixed $id
+     * @return \yii\web\Response
+     */
     public function run($id)
     {
         $model = $this->findModel($id);
@@ -78,12 +182,8 @@ class ActiveDownloadAction extends \yii\web\Action
             $model->getFilePath(),
             $model->getFileName(),
             [
-                'mimeType' => $model->mimeType(),
-                'inline' => !$this->forceDownload
-                    || !Yii::$app->request->get(
-                        $this->forceDownloadParam,
-                        false
-                    )
+                'mimeType' => $model->getMimeType(),
+                'inline' => !$this->forceDownload()
             ]
         );
     }
